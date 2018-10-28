66f1ee7381832e71aa50a14f71abda0364de1be3
Mason Hester <masonhester@Masons-MacBook-Pro.local>
2018-10-28 14:27:52 -0400

    Template for data storage

---
+++ portfolio.js
@@ -0,0 +1,22 @@
+const projects = [
+    {
+        name: "Google Minus",
+        language: "javascript",
+        githubLink: "https://github.com/DeclanMorrison/Band-Info-Aggregator",
+        liveLink: "https://declanmorrison.github.io/Band-Info-Aggregator/",
+        description: "This application was made to get information about a band (description, albums, songs, and lyrics) and compile them all in one place",
+        image: ""
+    }
+]
+
+const homeworks = [
+    {
+        name: "",
+        githubLink: "",
+        liveLink: "",
+        description: "",
+        toolsUsed: {
+            toolName: "link"
+        }
+    }
+]
